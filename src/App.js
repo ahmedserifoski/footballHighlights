f236b55b1e5473efd76e3565595dcfda3aedb04e
Ahmed Serifoski <ahmedserifoski@Ahmeds-MacBook-Air.local>
2022-06-01 07:56:05 +0200

    add league filtering

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ function App() {
   // const {footballData} = useContext(Context)  
 
   const [footballData, setFootballData] = useState([])
+  const [filteredLeague, setFilteredLeague] = useState([])
+
+  const filterLeague = (e) => {
+    setFilteredLeague(footballData.filter(highlight => (
+        highlight.competition.includes(e.target.value)
+    )))
+}
 
   useEffect(() => {
     const fetchFootballData = async () => {
@@ -26,14 +33,14 @@ function App() {
     
   }, [])
 
-  // console.log(footballData)
+  console.log(filteredLeague)
 
   return (
     <>
     <h1 className="bg-dark text-light mb-0 p-4 text-center">Football Highlights</h1>
-    <Categories footballData={footballData}/>
+    <Categories filterLeague={filterLeague} footballData={footballData}/>
     <div className="App pt-4">
-      {footballData.map(((highlight, i) => (
+      {filteredLeague.map(((highlight, i) => (
           <HighlightCard key={i} footballData={highlight} />
       )))}
     </div>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,31 +1,24 @@
 import React, {useState} from 'react'
-import {Badge} from "react-bootstrap"
+import {Badge, Button} from "react-bootstrap"
 
 //make a series of clickable items that filter out the results based od league clasifications:
     //make the buttons
     //add event listener
     //make a function that takes data from api and filters results
 
-const Categories = ({footballData}) => {
+const Categories = ({filterLeague}) => {
 
     const [competition, setCompetition] = useState()
-
-    const splittedData = footballData.filter(highlight => (
-        highlight.competition.includes("La Liga")
-    ))
-console.log(splittedData)
-    const filterLeague = (footballData) => {
-
-    }
+    
 
   return (
     <div className="bg-black text-center pt-4 pb-1 fs-3">
-        <span> <Badge bg="light" text="dark" onClick={filterLeague}>Serie A</Badge></span>
-        <span> <Badge bg="light" text="dark">Bundes Liga</Badge></span>
-        <span> <Badge bg="light" text="dark">Premier League</Badge></span>
-        <span> <Badge bg="light" text="dark">La Liga</Badge></span>
-        <span> <Badge bg="light" text="dark">Ligue 1</Badge></span>
-    </div>
+        <span> <Button value="ITALY" bg="light" text="dark" onClick={e => filterLeague(e)}>Italy</Button></span>
+        <span> <Button value="GERMANY" bg="light" text="dark" onClick={e => filterLeague(e)}>Germany</Button></span>
+        <span> <Button value="ENGLAND" bg="light" text="dark" onClick={e => filterLeague(e)}>England</Button></span>
+        <span> <Button value="FRANCE" bg="light" text="dark" onClick={e => filterLeague(e)}>France</Button></span>
+        <span> <Button value="SPAIN" bg="light" text="dark" onClick={e => filterLeague(e)}>Spain</Button></span>
+    /</div>
   )
 }
 
